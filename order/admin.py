4c44d320a8b3bdad6e99bad283e02cc507b4d7e7
Khushi Chawla <emailkhushic@gmail.com>
2020-07-17 13:19:12 -0400

    Created models

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Company, Agent, AgentShare, Order, Price
 
-# Register your models here.
+admin.site.register(Company)
+admin.site.register(Agent)
+admin.site.register(AgentShare)
+admin.site.register(Order)
+admin.site.register(Price)
