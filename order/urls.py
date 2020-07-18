b1a501998c984201ed4b74aed1596c06fed5d427
Chloe <chloee759486@gmail.com>
2020-07-17 21:02:32 -0400

    Changes to some of the orders files

--- order/urls.py
+++ order/urls.py
@@ -5,12 +5,6 @@ from . import views
 app_name = "order"
 
 urlpatterns = [
-<<<<<<< HEAD
-    path('', views.index , name='order-index'),
-    path('summary', views.summary , name='order-summary'),
-    
-=======
     #path('', views.OrderForm.post_new, name='order-index'),
     path('', views.OrderCreate.as_view(), name='order-index'),
->>>>>>> master
 ]
