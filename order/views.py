92f431ab95b2d2ebe8756bff6568e5180cd56071
Khushi Chawla <emailkhushic@gmail.com>
2020-07-23 15:50:28 -0400

    order filtering by logged in agent

--- order/views.py
+++ order/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 from django.views import generic
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.forms import ModelForm
 from django.urls import reverse_lazy
-from .models import Company, Agent, AgentShare, Order, Price
+from .models import *
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout
 
@@ -13,16 +12,28 @@ class OrderForm(ModelForm):
         model = Order
         fields = ['Direction', 'OrderBookName', 'Type', 'Price', 'Quantity']
 
+
+def getuser(request):
+    current_agent = Agent.objects.get(Agent=request.user)
+    return current_agent
+
 def orderpage(request):
     all_orders = orderfilter(request)
     all_companies = Company.objects.all()
     all_asks = all_orders.filter(Direction="A")
     all_bids = all_orders.filter(Direction="B")
-    all_orders = Order.objects.filter(Agent="alan")
+    all_orders = Order.objects.filter(Agent=getuser(request))
 
     form = OrderForm(request.POST or None)
     if form.is_valid():
-        form.save()
+        new_order = form.save(commit = False)
+        new_order.Agent = getuser(request)
+        new_order.QuantityToFill = new_order.Quantity
+        #if new_order.Type == "M" and new_order.Direction == "A":
+        #    new_order.Price = 0
+        #if new_order.Type == "M" and new_order.Direction == "B":
+        #    new_order.Price = 2147483647
+        new_order.save()
         form = OrderForm()
 
     context = {
@@ -35,7 +46,7 @@ def orderpage(request):
     return render(request, "order/orderpage.html", context)
 
 def orderfilter(request):
-    all_orders = Order.objects.filter(Agent="alan")
+    all_orders = Order.objects.filter(Agent=getuser(request))
     all_companies = Company.objects.all()
     companies = request.GET.get('company_form')
     if companies != "" and companies!= None:
