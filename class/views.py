9de789f732ea9dd2ee818fb129bd71484fa5f0b2
Khushi Chawla <emailkhushic@gmail.com>
2020-07-28 22:54:40 -0400

    rough graph on modal

--- class/views.py
+++ class/views.py
@@ -1,19 +1,26 @@
 from django.shortcuts import render, redirect
 from django.views import generic
-from django.forms import ModelForm
-from django.urls import reverse_lazy
-from .models import *
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout
 from order.models import *
-
+import json
+from django.http import HttpResponse
+from django.core import serializers
 
 def classview(request):
     all_companies = Company.objects.all()
-    depart_colors = {"Mathematics":"bg-primary", "Computer Science":"bg-light", "Applied Mathematics": "bg-dark"}
+    all_prices = Price.objects.all()
     context = {
         "all_classes" : all_companies,
-        "depart_colors" : depart_colors,
-        "Mathematics":"bg-primary",
     }
     return render(request, "class/classpage.html", context)
+
+def priceview(request):
+    if request.method == 'GET':
+        class_ticker = request.GET['class']
+        all_prices = Price.objects.filter(OrderBookName = class_ticker)
+        context = {
+            "all_prices" : all_prices
+        }
+        json_data = serializers.serialize('json', all_prices)
+        return HttpResponse(json_data, content_type="application/json")
+    else:
+        return HttpResponse("unsuccesful")
