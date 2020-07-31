28b78a2fd9e30c63f6e1f4f1cbc5d5e660498a0e
msboddu2020 <52417525+msboddu2020@users.noreply.github.com>
2020-07-31 12:11:29 -0400

    Added in verification link functionality

--- landingpage/urls.py
+++ landingpage/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register_url'),
     path('login/', views.login_request, name='login_url'),
     path('logout/', views.logout_request, name="logout_url"),
+    path('activate/<uidb64>/<token>', views.activate, name='activate'),
 ]
