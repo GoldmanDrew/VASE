a265234b6d32051f729d1774ac4acefc95555eb3
Som Mohapatra <sm2pvm@virginia.edu>
2020-08-16 15:09:13 -0400

    added FAQ content

--- landingpage/faqsfull.py
+++ landingpage/faqsfull.py
@@ -1,16 +1,17 @@
 faqdictionary = {
-"What is Have I Been Known?":
-"HIBK checks your bitcoin address for transactions with 'privacy gotchas' that make them more likely to be known to surveillance. Based on the results you are matched with relevant obfuscation resources that can help improve your bitcoin privacy.",
+"What is the VASE?":
+"We’re an exchange exclusively built for UVA students to buy and sell shares (or tokens) of things that you have an opinion on - think of buying a share as an upvote where 1) you have actual skin in the game and 2) you can make money from sharing your opinion. The UniExchange is all about relative valuation. For example, if we’re comparing one course vs. another, if one course has a lower price than another even when you think it should be higher, you buy a share (or multiple shares, if you’re really invested in that opinion). If enough people agree, that class’s share price will go up. The idea is, if enough of us participate in the market, buying when the share price is low (or simply, buying when we think it ought to be higher) and selling when it’s high (or ought to be lower), we as a community of students can be better informed of our opinion as a whole.",
 
-"How does HIBK work?":
-"A request is made to Blockstream's Esplora API to get recent transactions for your address. A response with JSON data for those transactions on the bitcoin blockchain is received. A privacy analysis script runs in your browser to identify any 'privacy gotchas' in the transactions.",
+"How does the VASE work?":
+"It’s simple supply-demand economics with a twist. In a normal market, and indeed in a normal stock exchange, when you buy, there needs to be a seller and when you sell, there needs to be a buyer - that doesn’t work very well when there’s not many buyers and sellers around! We’re pioneering a formula that ensures that you can buy and sell whenever you want - you’re exercising a vote here, not holding onto financial assets.",
 
-"Is HIBK tracking me?":
-"No. HIBK is a static website hosted on Github Pages so there is no server side processing. API requests, responses, and privacy analysis all take place locally in your browser. No tracking scripts. Everything is open source on GitHub.",
+"What can I get?":
+"You’ve got valuable information - your opinion. By buying (and later, selling) tokens of classes that you’ve taken, you get to exercise that opinion, changing the prices accordingly (buy a token to move up the price slightly, buy a lot of tokens to move up the price a lot). If you buy a token and other people agree with you, increasing its price, you can sell it for a profit. If you have experience trading stocks using technical analysis on public exchanges, you’ll likely be able to use the same strategies here. If you’re more experienced with fundamental analysis - reach out to us! We’re still figuring out a methodology for fundamentally valuing these assets.",
 
-"What is HIBK promoting?":
-"You will notice there are no reflinks or affiliate links, nor is there any advertising present on HIBK, and there never will be. At most, some merchandise may be offered through a separate online store linked to from the menu at some point in future. The intent of HIBK is to promote proper privacy practices that protect the fungibility of bitcoin.",
+"What does holding a “share” give me? What is the value of a share?":
+"These tokens aren’t financial assets with underlying cash flows, which is to say that they don’t hold value in the traditional stock market sense. But these shares are perfectly liquid, which means that you can buy or sell them at any point in time. This removal of barriers to exit or entry in the market means simply that buying a share gives you the right to sell it at any point. We think this is powerful because you, as an individual, can exercise a weighted upvote (a demand-side push) on an asset, pushing its price up and ranking it higher, or you can exercise a downvote (a supply-side push) whenever you think it has reached a high enough rank and want to sell it, pushing its price ever-so-slightly down.",
 
-"Why does HIBK link to certain exchanges?":
-"Exchanging bitcoins for other coins - or what is referred to as 'Chain Hopping' - is one method of obfuscating coins for privacy. Exchanges linked to by HIBK are non-KYC exchanges bst suited to such use-cases.",
+"Can I use the VASE without buying or selling tokens?":
+"If you don’t buy and sell shares, your opinion isn’t recorded in the market and in prices. But if you want to just see what other students think, you can absolutely use the VASE without buying/selling."
+
 }
